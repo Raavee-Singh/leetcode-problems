0a1e96443770fe14baf1211677ac93dc4c9115eb
Raavee Singh <gdg1465@gmail.com>
2026-08-22 07:25:17 +0530

    Implement twoSum function for sorted array

---
+++ Two-Sum-II-Input-Array-Is-Sorted.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> ans;
+        int n=numbers.size();
+        int start=0;
+        int end=n-1;
+        while(start<end){
+            int sum=numbers[start]+numbers[end];
+            if(sum==target){
+                ans.push_back(start+1);
+                ans.push_back(end+1);
+                return ans;
+            }
+            else if(sum>target){
+                end--;
+            }
+            else{
+                start++;
+            }
+
+        }
+        return {};
+    }
+};
